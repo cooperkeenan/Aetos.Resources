aee7144b8a21fd4e7bd66da49afc65dfca197dfc
Cooper Keenan <ckeenan@craneware.com>
2025-08-01 11:09:59 +0100

    fixed ssh error

--- azure-vm-infrastructure/modules/vm/main.tf
+++ azure-vm-infrastructure/modules/vm/main.tf
@@ -46,8 +46,8 @@ resource "azurerm_public_ip" "main" {
   name                = "${var.vm_name}-public-ip"
   resource_group_name = var.resource_group_name
   location            = var.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
 
   tags = {
     Environment = var.environment
